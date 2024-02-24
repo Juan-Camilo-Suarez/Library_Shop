8183ae2e13897268242ded3e227a1e54ce67f95e
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-02-24 19:13:39 +0300

    create view to get book by client

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
+    path('books', views.get_book_by_authors, name='get book by author pk'),
 
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.core.cache import cache
 
-# Create your views here.
+from app.models import Author
+
+# Cache time to live is 30 minutes.
+CACHE_TTL = 60 * 30
+
+
+@api_view()
+def get_book_by_authors(request):
+    """http://127.0.0.1:8000/api/weather?pk=1"""
+    author_pk = request.GET.get('pk')
+    my_value = cache.get(author_pk)
+    data = {}
+    if my_value is None:
+        author = Author.objects.prefetch_related('books').get(pk=author_pk)
+        for a in author.books.all():
+            data[a.id] = a.name
+
+        cache.set(author_pk, data, CACHE_TTL)
+        return Response(data)
+    else:
+        return Response(my_value)
