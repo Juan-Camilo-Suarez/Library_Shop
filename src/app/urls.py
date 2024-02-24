7110395ae8ccfcfa524c7ce579460164e09b691e
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-02-24 22:52:14 +0300

    create view to get order

--- src/app/urls.py
+++ src/app/urls.py
@@ -5,6 +5,7 @@ from app import views
 
 urlpatterns = [
     path('books', views.get_book_by_authors, name='get book by author pk'),
-    path('orders', views.get_orders_by_client, name='get orders_by_client')
+    path('orders', views.get_orders_by_client, name='get orders_by_client'),
+    path('order', views.get_order_info, name='get order')
 
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.core.cache import cache
 
-from app.models import Author, Client
+from app.models import Author, Client, Order
 
 # Cache time to live is 30 minutes.
 CACHE_TTL = 60 * 30
@@ -28,7 +28,7 @@ def get_book_by_authors(request):
 
 @api_view()
 def get_orders_by_client(request):
-    """http://127.0.0.1:8000/api/weather?pk=1"""
+    """http://127.0.0.1:8000/api/v1/books?pk=1"""
     client_pk = request.GET.get('pk')
     key_cache = f"orders-{client_pk}"
     my_client = cache.get(key_cache)
@@ -40,6 +40,27 @@ def get_orders_by_client(request):
                 "employee": cli.employee.name,
                 "book": cli.book.name
             }
+
+        cache.set(key_cache, data, CACHE_TTL)
+        return Response(data)
+    else:
+        return Response(my_client)
+
+
+@api_view()
+def get_order_info(request):
+    """http://127.0.0.1:8000/api/v1/orders?pk=2"""
+    order_pk = request.GET.get('pk')
+    key_cache = f"order-{order_pk}"
+    my_client = cache.get(key_cache)
+    data = {}
+    if my_client is None:
+        order = Order.objects.select_related().get(pk=order_pk)
+        data[order.id] = {
+            "employee": order.employee.name,
+            "book": order.book.name,
+            "client": order.client.name
+        }
 
         cache.set(key_cache, data, CACHE_TTL)
         return Response(data)
