bae56fb47c1f62298fcd7b976cf068144f939add
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-02-24 15:36:23 +0300

    add models in admin

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from app.models import Client, Employee, Author, Book, Order
+
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Employee)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Order)
