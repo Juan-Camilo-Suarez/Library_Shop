292d0e899d108080a941976f5eb0764c5a224eaf
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-03-02 10:33:43 +0300

    + create router and include author

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from app import views
+
+router = routers.SimpleRouter()
+
+router.register('author', views.AuthorViewSetModel)
 
 urlpatterns = [
     path('books', views.get_book_by_authors, name='get book by author pk'),
     path('orders', views.get_orders_by_client, name='get orders_by_client'),
-    path('order', views.get_order_info, name='get order')
-
+    path('order', views.get_order_info, name='get order'),
+    path('', include(router.urls)),
 ]
