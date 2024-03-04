0486ef5895da6a848d76315f934a3bf4dff94700
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-03-04 23:20:23 +0300

    + add config to celery with redis

--- src/project/settings.py
+++ src/project/settings.py
@@ -131,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
